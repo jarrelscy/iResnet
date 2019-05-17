fa22fbdc86e1e85d161642e4287d06e5e4b2436c
Jarrel Seah <jarrelscy@gmail.com>
2019-05-17 16:00:23 +1000

    Update SpectralNormGouk.py

--- SpectralNormGouk.py
+++ SpectralNormGouk.py
@@ -26,36 +26,7 @@ class SpectralNorm(object):
         self.eps = eps
     def l2norm(self, t):
         return torch.sqrt((t ** 2).sum())
-    def compute_weight(self, module, do_power_iteration, num_iter=0):
-        # NB: If `do_power_iteration` is set, the `u` and `v` vectors are
-        #     updated in power iteration **in-place**. This is very important
-        #     because in `DataParallel` forward, the vectors (being buffers) are
-        #     broadcast from the parallelized module to each module replica,
-        #     which is a new module object created on the fly. And each replica
-        #     runs its own spectral norm power iteration. So simply assigning
-        #     the updated vectors to the module this function runs on will cause
-        #     the update to be lost forever. And the next time the parallelized
-        #     module is replicated, the same randomly initialized vectors are
-        #     broadcast and used!
-        #
-        #     Therefore, to make the change propagate back, we rely on two
-        #     important bahaviors (also enforced via tests):
-        #       1. `DataParallel` doesn't clone storage if the broadcast tensor
-        #          is alreay on correct device; and it makes sure that the
-        #          parallelized module is already on `device[0]`.
-        #       2. If the out tensor in `out=` kwarg has correct shape, it will
-        #          just fill in the values.
-        #     Therefore, since the same power iteration is performed on all
-        #     devices, simply updating the tensors in-place will make sure that
-        #     the module replica on `device[0]` will update the _u vector on the
-        #     parallized module (by shared storage).
-        #
-        #    However, after we update `u` and `v` in-place, we need to **clone**
-        #    them before using them to normalize the weight. This is to support
-        #    backproping through two forward passes, e.g., the common pattern in
-        #    GAN training: loss = D(real) - D(fake). Otherwise, engine will
-        #    complain that variables needed to do backward for the first forward
-        #    (i.e., the `u` and `v` vectors) are changed in the second forward.
+    def compute_weight(self, module, do_power_iteration, num_iter=0):      
         weight = getattr(module, self.name + '_orig')
         u = getattr(module, self.name + '_u')
         
